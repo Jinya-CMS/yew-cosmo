cb4ae49d2838c4fa97ee1835f5522bd99aad98bc
Imanuel Ulbricht <me@imanuel.dev>
2023-07-28 14:38:50 +0200

    Update about install section to match crate

--- docs/src/pages/cosmo/about.rs
+++ docs/src/pages/cosmo/about.rs
@@ -5,8 +5,8 @@ use yew_cosmo::prelude::*;
 pub fn about_cosmo() -> Html {
     html!(
         <>
-            <CosmoTitle title="Cosmo CSS by Jinya" />
-            <CosmoHeader level={CosmoHeaderLevel::H2} header="What is Cosmo CSS" />
+            <CosmoTitle title="Cosmo CSS (for Yew) by Jinya" />
+            <CosmoHeader level={CosmoHeaderLevel::H2} header="What is Cosmo CSS (for Yew)" />
             <CosmoParagraph>
                 {"Cosmo CSS is a small CSS library that gives you the ability to style your applications
                 following the cosmopolitan design guidelines. These guidelines were developed by
@@ -21,7 +21,7 @@ pub fn about_cosmo() -> Html {
             <CosmoParagraph>
                 {"If you need a CSS library for your next web application cosmo might be the right fit."}
             </CosmoParagraph>
-            <CosmoHeader level={CosmoHeaderLevel::H2} header="What Cosmo CSS is not" />
+            <CosmoHeader level={CosmoHeaderLevel::H2} header="What Cosmo CSS (for Yew) is not" />
             <CosmoParagraph>
                 {"The cosmopolitan design was developed by Microsoft for desktop and Silverlight
                 applications and is not made for websites in the classical sense. Even though we
@@ -30,8 +30,7 @@ pub fn about_cosmo() -> Html {
             </CosmoParagraph>
             <CosmoHeader level={CosmoHeaderLevel::H2} header="How to install" />
             <CosmoParagraph>
-                {"You can either just drop the CSS files in your project or install the npm package "}
-                <CosmoCode>{"@jinyacms/cosmo"}</CosmoCode>
+                {"Installing Cosmo CSS (for Yew) is simple, just add the crate  "}<CosmoCode>{"yew-cosmo"}</CosmoCode>{" to your Cargo.toml and you are good to go."}
             </CosmoParagraph>
             <CosmoHeader level={CosmoHeaderLevel::H2} header="Found a bug?" />
             <CosmoParagraph>
@@ -39,7 +38,7 @@ pub fn about_cosmo() -> Html {
                 <CosmoAnchor href="https://taiga.imanuel.dev/project/cosmo-css/">{"https://taiga.imanuel.dev/project/cosmo-css/"}</CosmoAnchor>
             </CosmoParagraph>
             <CosmoHeader level={CosmoHeaderLevel::H2} header="License" />
-            <CosmoParagraph>{"Like all other Jinya projects, Cosmo CSS is distributed under the MIT License."}</CosmoParagraph>
+            <CosmoParagraph>{"Like all other Jinya projects, Cosmo CSS (for Yew) is distributed under the MIT License."}</CosmoParagraph>
         </>
     )
 }
